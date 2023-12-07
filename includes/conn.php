19eeb6058a5c47c4ce08037eb9a44e24256f4b59
Aksita-G <113622172+Aksita-G@users.noreply.github.com>
2023-12-07 19:45:07 +0530

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+	$conn = new mysqli('voting.mysql.database.azure.com', 'azureuser', 'database@2023', 'voting');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
